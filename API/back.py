964b2bd0fb9cc976500fc25c485f35646c3368f5
jjzapata99 <jjzapata@espol.edu.ec>
2022-11-24 11:52:07 -0500

    Server datos

--- API/back.py
+++ API/back.py
@@ -1,8 +1,10 @@
+
 from pymongo import MongoClient
 import pandas as pd
 import datetime
+import pytz
 
-client = MongoClient('mongodb://0.0.0.0:8801/')
+client = MongoClient('mongodb://10.0.1.71:8801/')
 db_scan= client['scan']
 c_temperature= db_scan['temperature']
 c_address = db_scan['address']
@@ -14,11 +16,11 @@ def exist(ip):
 def updateIp(ip='0.0.0.0'):
     ex =exist(ip)
     if(ex==0):
-        c_address.insert_one({ "_id": ip ,"nombre": "Juan", "direccion": "C/ Mayor 1" })
+        c_address.insert_one({ "_id": ip ,"sensedAt": datetime.datetime.now(pytz.utc) })
 def deleteIp(ip='0.0.0.0'):
     ex =exist(ip)
     if(ex>0):
         c_address.delete_one({'_id': ip})
 
-def sensor(degree= 0):
-    c_temperature.insert_one({'createAt':datetime.datetime.now(), 'temperature': degree})
+def sensor(degree='0'):
+    c_temperature.insert_one({'createAt':datetime.datetime.now(pytz.utc), 'temperature': degree})
--- API/main.py
+++ API/main.py
@@ -17,12 +17,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.post('/address/on')
-async def post(ip : str ):
+@app.get('/address/on')
+async def get(ip : str ):
     updateIp(ip)
-@app.post('/address/off')
-async def post(ip : str ):
+@app.get('/address/off')
+async def get(ip : str ):
     deleteIp(ip)
-@app.post('/temperature')
-async def post(degrees : int ):
+@app.get('/temperature')
+async def get(degrees : str):
     sensor(degrees)
