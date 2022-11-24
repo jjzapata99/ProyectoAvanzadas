dc350bb0911a2edf090ab48cd28e314dac6b5d4b
jjzapata99 <jjzapata@espol.edu.ec>
2022-11-24 12:07:44 -0500

    solved

--- API/back.py
+++ API/back.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 from pymongo import MongoClient
 import pandas as pd
 import datetime
@@ -25,30 +23,3 @@ def deleteIp(ip='0.0.0.0'):
 
 def sensor(degree='0'):
     c_temperature.insert_one({'createAt':datetime.datetime.now(pytz.utc), 'temperature': degree})
-=======
-from pymongo import MongoClient
-import pandas as pd
-import datetime
-
-client = MongoClient('mongodb://200.126.14.228:8801/')
-db_scan= client['scan']
-c_temperature= db_scan['temperature']
-c_address = db_scan['address']
-def exist(ip):
-    query={'_id':ip}
-    dt_q=pd.DataFrame(list(c_address.find(query)))
-    return  len(dt_q)
-
-def updateIp(ip='0.0.0.0'):
-    ex =exist(ip)
-    if(ex==0):
-        c_address.insert_one({ "_id": ip ,"nombre": "Juan", "direccion": "C/ Mayor 1" })
-def deleteIp(ip='0.0.0.0'):
-    ex =exist(ip)
-    if(ex>0):
-        c_address.delete_one({'_id': ip})
-
-def sensor(degree= 0):
-    c_temperature.insert_one({'createAt':datetime.datetime.now(), 'temperature': degree})
-updateIp('200.126.14.202')
->>>>>>> c03b4f8938aacc676845b68a4a0afd9b11707e89
--- API/main.py
+++ API/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from back import *
@@ -27,33 +26,4 @@ async def get(ip : str ):
 @app.get('/temperature')
 async def get(degrees : str):
     sensor(degrees)
-=======
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from back import *
-app = FastAPI(
-    title= 'Monitoreo', description= 'Api para la carga de datos de monitoreo', version= '1.1.0'
-)
-origins = [
-    "http://localhost",
-    "http://localhost:4200",
-    "http://localhost:4200/*",
-    "https://web.postman.co/*"
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-@app.get('/address/on')
-async def get(ip : str ):
-    updateIp(ip)
-@app.post('/address/off')
-async def post(ip : str ):
-    deleteIp(ip)
-@app.post('/temperature')
-async def post(degrees : int ):
-    sensor(degrees)
->>>>>>> c03b4f8938aacc676845b68a4a0afd9b11707e89
+
