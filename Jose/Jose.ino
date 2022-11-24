c03b4f8938aacc676845b68a4a0afd9b11707e89
jjzapata99 <jjzapata@espol.edu.ec>
2022-11-24 11:55:47 -0500

    final

--- Jose/Jose.ino
+++ Jose/Jose.ino
@@ -1,67 +1,100 @@
-#include <HTTPClient.h>
 #include <WiFi.h>
+#include <ESP32Ping.h>
+#include <stdio.h>
+#include <HTTPClient.h>
 
-//si usas esp8266
-//#include <ESP8266WiFiMulti.h>
-//#include <ESP8266HTTPClient.h>
+#define ADC_VREF_mV    3300.0 
+#define ADC_RESOLUTION 4096.0
+#define PIN_LM35       35
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
 
 const char* ssid = "Lab_Avanzadas";
 const char* password =  "la1bo2ra3to4ri5o";
 
 
-void setup() {
-  delay(10);
-  Serial.begin(115200);
-
-
-  WiFi.begin(ssid, password);
-
-  Serial.print("Conectando...");
-  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
-    delay(500);
-    Serial.print(".");
+void Task1code(void * pvParameters){
+  
+  for(;;){
+  WiFiClient client;
+  HTTPClient http;
+  int x =200 ;
+  
+  while (x <= 255){
+    String comando = "200.126.14." + String (x);
+    Serial.println(comando);
+    bool success = Ping.ping( comando.c_str(), 1);  
+    x+=1;
+    if(!success){
+    Serial.println("Ping failed");
+    http.begin(client, "http://200.126.14.228:8800/address/off?ip="+comando);
+    http.GET();
+    
+  }
+  else{ 
+  Serial.println("Ping succesful.");
+ 
+  http.begin(client, "http://200.126.14.228:8800/address/on?ip="+comando);
+  http.GET();
+  
+  }
+ 
   }
 
-  Serial.print("Conectado con éxito, mi IP es: ");
-  Serial.println(WiFi.localIP());
-
+   
+}
+}
+void Task2code(void * pvParameters){
+  for(;;){
+  WiFiClient client;
+  HTTPClient http;
+   
+    int adcVal = analogRead(PIN_LM35);
+    float milliVolt = adcVal * (ADC_VREF_mV / ADC_RESOLUTION);
+    float tempC = milliVolt / 10;
+    Serial.println(tempC);
+    http.begin(client, "http://200.126.14.228:8800/temperature?degrees="+String(tempC));
+    http.GET();
+    delay(1500);
+}
 }
 
-void loop() {
 
-  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
 
-    HTTPClient http;
-    String datos_a_enviar = "?ip=200.126.1.4";
 
-    http.begin("http://200.126.14.228:8800/address/on");        //Indicamos el destino
 
-    int codigo_respuesta = http.POST(datos_a_enviar);   //Enviamos el post pasándole, los datos que queremos enviar. (esta función nos devuelve un código que guardamos en un int)
 
-    if(codigo_respuesta>0){
-      Serial.println("Código HTTP ► " + String(codigo_respuesta));   //Print return code
-
-      if(codigo_respuesta == 200){
-        String cuerpo_respuesta = http.getString();
-        Serial.println("El servidor respondió ▼ ");
-        Serial.println(cuerpo_respuesta);
-
-      }
-
-    }else{
-
-     Serial.print("Error enviando POST, código: ");
-     Serial.println(codigo_respuesta);
-
-    }
-
-    http.end();  //libero recursos
-
-  }else{
-
-     Serial.println("Error en la conexión WIFI");
-
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
   }
 
-   delay(2000);
+  xTaskCreatePinnedToCore(
+                    Task1code,  
+                    "scan",   
+                    10000,     
+                    NULL,       
+                    1,          
+                    &Task1,     
+                    0);                            
+  delay(500); 
+  xTaskCreatePinnedToCore(
+                    Task2code, 
+                    "sense",    
+                    10000,       
+                    NULL,        
+                    1,           
+                    &Task2,     
+                    1);          
+    delay(5000); 
+}
+  
+ 
+ 
+void loop() {
+  
 }
